a4db3d22e15a7caf21b68820362f28fb44c54270
maxper4 <maxenceperion@gmail.com>
2023-08-15 00:07:09 +0200

    Implemented command to report an event

--- x/cosmap/keeper/msg_server_report_event.go
+++ x/cosmap/keeper/msg_server_report_event.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/maxper4/cosmap/x/cosmap/types"
@@ -10,8 +11,29 @@ import (
 func (k msgServer) ReportEvent(goCtx context.Context, msg *types.MsgReportEvent) (*types.MsgReportEventResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
 
-	return &types.MsgReportEventResponse{}, nil
+	newEvent := types.Events{
+		Index:     newIndex,
+		Position:  msg.Position,
+		Sender:    msg.Creator,
+		Event:     msg.Event,
+		Timestamp: 0, // TODO: Set timestamp
+	}
+
+	// err := newEvent.Validate()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	k.SetEvents(ctx, newEvent)
+
+	systemInfo.NextId++
+	k.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgReportEventResponse{EventId: newIndex}, nil
 }
